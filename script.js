e770a3e2d41efc59ab85be3f1d48754586d7cebc
RobertAKARobin <robertgfthomas@gmail.com>
2016-06-01 13:07:03 -0400

    Shows me if answer was correct

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ $(document).ready(function(){
         $(".board").append(randomProblemUpTo(10));
         problemNumber += 1;
     }
+    
+    $("input").on("change", function(){
+        var el = $(this);
+        var answer = el.attr("secret-answer");
+        var guess = el.val();
+        if(guess == answer){
+            el.css("background-color", "#afa");
+        }else{
+            el.css("background-color", "#faa");
+        }
+    });
 
     function randomNumberUpTo(maximum){
         return Math.round(Math.random() * maximum);
@@ -16,7 +27,7 @@ $(document).ready(function(){
         var firstNumber = randomNumberUpTo(maximum);
         var secondNumber = randomNumberUpTo(maximum);
         var sum = (firstNumber + secondNumber);
-        return ("<h2>" + firstNumber + " + " + secondNumber + " = " + sum + "</h2>");
+        return ("<h2>" + firstNumber + " + " + secondNumber + " = " + "<input secret-answer='" + sum + "' />" + "</h2>");
     }
 
 });
