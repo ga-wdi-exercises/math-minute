3583314ae6ac15cc033b2cdf07c348473ea34cc0
RobertAKARobin <robertgfthomas@gmail.com>
2016-06-01 12:52:07 -0400

    Generates random math problem

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    $("body").css("background-color", "red");
+
+    var firstNumber = Math.round(Math.random() * 10);
+    var secondNumber = Math.round(Math.random() * 10);
+    var sum = (firstNumber + secondNumber);
+    $(".board").append("<h2>" + firstNumber + " + " + secondNumber + " = " + sum + "</h2>");
+
 });
