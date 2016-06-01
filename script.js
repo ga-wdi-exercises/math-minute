c85359388f0117fff12a21354912ac0906903bbc
RobertAKARobin <robertgfthomas@gmail.com>
2016-06-01 13:00:23 -0400

    Generates any number of random additionl problems

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
 
-    var firstNumber = Math.round(Math.random() * 10);
-    var secondNumber = Math.round(Math.random() * 10);
-    var sum = (firstNumber + secondNumber);
-    $(".board").append("<h2>" + firstNumber + " + " + secondNumber + " = " + sum + "</h2>");
+    var totalProblems = 60;
+    var problemNumber = 0;
+    
+    while(problemNumber < totalProblems){
+        $(".board").append(randomProblemUpTo(10));
+        problemNumber += 1;
+    }
+
+    function randomNumberUpTo(maximum){
+        return Math.round(Math.random() * maximum);
+    }
+    
+    function randomProblemUpTo(maximum){
+        var firstNumber = randomNumberUpTo(maximum);
+        var secondNumber = randomNumberUpTo(maximum);
+        var sum = (firstNumber + secondNumber);
+        return ("<h2>" + firstNumber + " + " + secondNumber + " = " + sum + "</h2>");
+    }
 
 });
